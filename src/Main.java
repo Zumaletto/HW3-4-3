0e786426b85deb423745ded32ca1e57c8261c75a
Kirillova Olesya <olesyak-85@mail.ru>
2021-08-18 12:30:50 +0300

    modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
+      //  int creditAmount = 1_000_000;
+      //  System.out.println("Размер кредита :" + creditAmount);
+      //  int years = 1;
+      //  System.out.println("Срок кредитования :" + years);
+     //   double rate = 9.99;
+     //   System.out.println("Процентная ставка :" + rate);
 
         int monthlyPayment = service.calculate(1_000_000, 1, 9.99);
         System.out.println("Ежемесячный платеж :" + monthlyPayment);
